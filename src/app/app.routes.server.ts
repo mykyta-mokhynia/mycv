6a801c92e59d46bc565bf07281fc1f7aa618da17
mykyta-mokhynia <mykytamokhynia@gmail.com>
2025-08-14 03:48:08 -0300

    Fix Netlify compatibility: update server.ts for AppEngine and fix prerendering params

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: ':lang',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => [
+      { lang: 'ua' },
+      { lang: 'en' }
+    ]
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
